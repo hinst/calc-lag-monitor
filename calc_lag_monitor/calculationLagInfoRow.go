e2fae6fb9844f3cbddcabc9f7a6e3559e8c37042
Alexander Savinykh <asavinykh@nbrx.de>
2021-10-16 20:46:36 +0200

    Adding function: read calculation lag info row

--- calc_lag_monitor/calculationLagInfoRow.go
+++ calc_lag_monitor/calculationLagInfoRow.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"errors"
+	"strconv"
 	"time"
 )
 
@@ -16,4 +18,17 @@ func (row *CalculationLagInfoRow) Write(buffer *bytes.Buffer) {
 	BinaryWrite(buffer, row.Time.UnixMilli())
 	row.Cheap.Write(buffer)
 	row.Expensive.Write(buffer)
+}
+
+func (row *CalculationLagInfoRow) Read(buffer *bytes.Buffer) {
+	var version BinaryObjectVersionNumber
+	BinaryRead(buffer, &version)
+	if version != 1 {
+		panic(errors.New("Expected version 1 but got " + strconv.Itoa(int(version))))
+	}
+	var timeUnixMilli int64
+	BinaryRead(buffer, &timeUnixMilli)
+	row.Time = time.UnixMilli(timeUnixMilli)
+	row.Cheap.Read(buffer)
+	row.Expensive.Read(buffer)
 }
