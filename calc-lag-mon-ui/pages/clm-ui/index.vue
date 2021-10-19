3b509449f36e11214e74c9892142fc4243cf0fa5
Alexander Savinykh <asavinykh@nbrx.de>
2021-10-19 22:51:35 +0200

    Define UI pages; work in progress

---
+++ calc-lag-mon-ui/pages/clm-ui/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Hello</h1>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+    mounted() {
+        console.log('mounted');
+    }
+});
+</script>
---
+++ calc-lag-mon-ui/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Hello outer</h1>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+    mounted() {
+        console.log('mounted');
+    }
+});
+</script>
