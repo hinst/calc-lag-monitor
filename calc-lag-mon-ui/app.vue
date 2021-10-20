fdbb0aa1c4f901a4d5ec67f8e70d801398f97c44
Alexander Savinykh <asavinykh@nbrx.de>
2021-10-20 13:04:02 +0200

    Replaced Nuxt 3 project with Nuxt 2 project

--- calc-lag-mon-ui/app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <div class="w3-container w3-black">
-        <h4>Calculation Lag Monitor</h4>
-    </div>
-    <NuxtPage/>
-  </div>
-</template>
--- calc-lag-mon-ui/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineNuxtConfig } from 'nuxt3'
-
-export default defineNuxtConfig({
-    css: [
-        '@/assets/css/w3.css',
-        '@/assets/css/app.css'
-    ]
-})
