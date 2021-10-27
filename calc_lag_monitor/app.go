23a630c578e7cba9bf4f94cc7cd746f50ccf4bf0
Alexander Savinykh <asavinykh@nbrx.de>
2021-10-28 00:44:26 +0200

    Refactoring -> split Run() into sub-functions

--- calc_lag_monitor/app.go
+++ calc_lag_monitor/app.go
@@ -14,27 +14,9 @@ type App struct {
 }
 
 func (app *App) Run() {
-	if app.Storage == nil {
-		app.Storage = &DataStorage{}
-		app.Storage.Open()
-	}
-	if app.Monitor == nil {
-		app.Monitor = &CalculationLogMonitor{
-			Configuration: LoadConfiguration(),
-			Storage:       app.Storage,
-			LogEnabled:    true,
-		}
-	}
-	if app.Provider == nil {
-		app.Provider = &DataProvider{Storage: app.Storage}
-		app.Provider.Register()
-		listener, listenerError := net.Listen("tcp", ":3006")
-		AssertWrapped(listenerError, "Unable to listen")
-		go func() {
-			error := http.Serve(listener, nil)
-			AssertWrapped(error, "Unable to serve")
-		}()
-	}
+	app.InitializeStorage()
+	app.InitializeMonitor()
+	app.InitializeProvider()
 	if app.Exiting == nil {
 		app.Exiting = make(chan bool)
 	}
@@ -45,6 +27,36 @@ func (app *App) Run() {
 	app.Monitor.Wait()
 	app.Storage.Close()
 	log.Print("Shutdown process is now complete")
+}
+
+func (app *App) InitializeStorage() {
+	if app.Storage == nil {
+		app.Storage = &DataStorage{}
+		app.Storage.Open()
+	}
+}
+
+func (app *App) InitializeMonitor() {
+	if app.Monitor == nil {
+		app.Monitor = &CalculationLogMonitor{
+			Configuration: LoadConfiguration(),
+			Storage:       app.Storage,
+			LogEnabled:    true,
+		}
+	}
+}
+
+func (app *App) InitializeProvider() {
+	if app.Provider == nil {
+		app.Provider = &DataProvider{Storage: app.Storage}
+		app.Provider.Register()
+		listener, listenerError := net.Listen("tcp", ":3006")
+		AssertWrapped(listenerError, "Unable to listen")
+		go func() {
+			error := http.Serve(listener, nil)
+			AssertWrapped(error, "Unable to serve")
+		}()
+	}
 }
 
 func (app *App) Shutdown() {
