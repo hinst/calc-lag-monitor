806d0b5b90ef73d187c0a48dfb33b3175ef56423
Alexander Savinykh <asavinykh@nbrx.de>
2021-11-01 00:11:26 +0100

    Display chart -> is now operational

---
+++ calc-lag-mon-ui/components/CalculationLagChart.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+import { defineComponent } from '@nuxtjs/composition-api';
+import { Line, mixins } from 'vue-chartjs';
+
+export default defineComponent({
+  extends: Line,
+  mixins: [mixins.reactiveProp],
+  props: {
+    options: {
+      type: Object
+    }
+  },
+  mounted() {
+    (this as any).renderChart(this.chartData, this.options);
+  }
+})
+
+</script>
--- calc-lag-mon-ui/pages/calculation-lag-chart.vue
+++ calc-lag-mon-ui/pages/calculation-lag-chart.vue
@@ -1,19 +1,85 @@
 <template>
   <div>
-    calculation lag chart
+    <div v-if="chartData" style="max-width: 100%">
+      <CalculationLagChart :height="500" :chart-data="chartData" :options="chartOptions" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, useContext } from '@nuxtjs/composition-api'
+import { defineComponent, ref, useContext } from '@nuxtjs/composition-api';
+import * as lodash from 'lodash';
+
+interface CalculationLagItem {
+  Min: number;
+  Average: number;
+  Max: number;
+}
+
+interface CalculationLagRow {
+  Time: string;
+  Cheap: CalculationLagItem;
+  Expensive: CalculationLagItem;
+}
 
 export default defineComponent({
   setup() {
     const context = useContext();
+    const chartData = ref<any>(null);
+    const chartOptions = ref<any>(
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+              displayFormats: {
+                'millisecond': 'MMM DD',
+                'second': 'MMM DD',
+                'minute': 'MMM DD',
+                'hour': 'MMM DD',
+                'day': 'MMM DD',
+                'week': 'MMM DD',
+                'month': 'MMM DD',
+                'quarter': 'MMM DD',
+                'year': 'MMM DD',
+              }
+            },
+            gridLines: {
+              color: 'rgba(200, 200, 200, 0.5)'
+            }
+          }],
+          yAxes: [{
+            gridLines: {
+              color: 'rgba(200, 200, 200, 0.5)'
+            },
+            ticks: {
+              min: 0,
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    );
     setTimeout(async () => {
       const response = await fetch(context.env.apiUrl + '/clm/lag');
-      console.log(response.statusText);
-    }, 2000);
+      let responseArray: CalculationLagRow[] = JSON.parse(await response.text());
+      responseArray = lodash.sortBy(responseArray, row => new Date(row.Time));
+      const data = responseArray.map(a => ({ x: new Date(a.Time), y: a.Cheap.Average / 1000_000_000 }));
+      chartData.value = {
+        labels: responseArray.map(a => new Date(a.Time)),
+        datasets: [{
+          label: 'average cheap',
+          data: data,
+          backgroundColor: 'rgba(200, 200, 200, 0.5)'
+        }]
+      };
+    }, 1000);
+    return {
+      chartData,
+      chartOptions
+    };
   },
 });
 </script>
