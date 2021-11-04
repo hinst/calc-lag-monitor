2a33c91eaab43155433eaf856c13fdbbba4a3b98
Alexander Savinykh <asavinykh@nbrx.de>
2021-11-04 12:24:54 +0100

    Added appImporter to merge two database files

--- calc_lag_monitor/main.go
+++ calc_lag_monitor/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "runtime/debug"
+import (
+	"flag"
+	"runtime/debug"
+)
 
 func main() {
-	debug.SetGCPercent(50)
 	println("STARTING...")
-	var app App
-	app.Run()
+	debug.SetGCPercent(50)
+	importEnabled := flag.Bool("import", false, "Import database file")
+	db := flag.String("db", "", "File path")
+	flag.Parse()
+	if *importEnabled {
+		var appImporter AppImporter
+		appImporter.SourceDbFilePath = *db
+		appImporter.Run()
+	} else {
+		var app App
+		app.Run()
+	}
 	println("...EXITING")
 }
