3de7928ef64c5c733dc836c00e4d361ee91e425c
Alexander Savinykh <asavinykh@nbrx.de>
2021-11-17 00:07:35 +0100

    Added function BytesToString

--- calc_lag_monitor/string.go
+++ calc_lag_monitor/string.go
@@ -1,6 +1,9 @@
 package main
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 func ParseIntOr0(text string) (result int64) {
 	if len(text) > 0 {
@@ -9,4 +12,12 @@ func ParseIntOr0(text string) (result int64) {
 		AssertWrapped(parseError, "Cannot parse text as integer '"+text+"'")
 	}
 	return
+}
+
+func BytesToString(bytes []byte) string {
+	textParts := make([]string, len(bytes))
+	for index, value := range bytes {
+		textParts[index] = strconv.Itoa(int(value))
+	}
+	return strings.Join(textParts, " ")
 }
